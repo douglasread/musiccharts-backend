f57249c6ebb62e11521ea5bf3044f7ec2b4761ca
Douglas Read <dougread11@gmail.com>
2023-12-15 12:03:25 -0500

    initial commit

---
+++ src/main/java/com/musiccharts/musicchartsbackend/MusicchartsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.musiccharts.musicchartsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicchartsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicchartsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/musiccharts/musicchartsbackend/MusicchartsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musiccharts.musicchartsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicchartsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
